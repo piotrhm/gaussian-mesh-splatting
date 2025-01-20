7aced819e2662089b4a07bf1bd1edccecda2b7ee
piotrhm <helm.piotr@gmail.com>
2025-01-20 18:35:47 +0100

    fix-2-7

--- train.py
+++ train.py
@@ -172,12 +172,11 @@ def training_report(tb_writer, iteration, Ll1, loss, l1_loss, elapsed, testing_i
         torch.cuda.empty_cache()
         validation_configs = (
             {'name': 'val', 'cameras' : scene.getValCameras()},
-            {'name': 'test', 'cameras' : scene.getTestCameras()},
+            #{'name': 'test', 'cameras' : scene.getTestCameras()},
         )
 
         for config in validation_configs:
             if config['cameras'] and len(config['cameras']) > 0:
-                print(config)
                 l1_test = 0.0
                 psnr_test = 0.0
                 ssim_test = 0.0
